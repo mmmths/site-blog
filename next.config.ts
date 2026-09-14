8f0e91df26ebba1965444c965afbf3b39d3e0f65
Matheus Veiga Lima <mveigadelima@gmail.com>
2026-09-14 16:31:32 -0300

    feat: creating blog section

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
+import { withContentlayer } from "next-contentlayer";
 
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   reactCompiler: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  turbopack: {}
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
