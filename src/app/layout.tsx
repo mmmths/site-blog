dfc13ca8ef549e05d87d86f44d482790a081c86e
Matheus Veiga Lima <mveigadelima@gmail.com>
2026-09-24 09:16:04 -0300

    feat: add layout and landing page components with routing support

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { Layout } from "@/components/layout";
+import "@/styles/globals.css";
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Layout>{children}</Layout>
+      </body>
+    </html>
+  );
+}
--- src/components/active-link/active-link.tsx
+++ src/components/active-link/active-link.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 type ActiveLinkProps = React.ComponentProps<typeof Link>;
 
@@ -10,9 +12,10 @@ export function ActiveLink({
   className,
   ...rest
 }: ActiveLinkProps) {
-  const router = useRouter();
-
-  const isCurrentLink = router.asPath === href || router.asPath === rest.as;
+  const pathName = usePathname();
+  const linkPath = (typeof href === "string" ? href : href.pathname) ?? "";
+  const isActive =
+    pathName === linkPath || pathName?.startsWith(`${linkPath}/`);
 
   return (
     <Link
@@ -20,7 +23,7 @@ export function ActiveLink({
       {...rest}
       className={cn(
         "text-action-sm transition-colors hover:text-blue-200",
-        isCurrentLink ? "text-blue-500" : "text-gray-100",
+        isActive ? "text-blue-500" : "text-gray-100",
         className
       )}
     >
