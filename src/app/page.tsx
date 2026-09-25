0d68634339bf16545883da6589d7030fb849d42f
Matheus Veiga Lima <mveigadelima@gmail.com>
2026-09-25 09:00:48 -0300

    feat: add metadata for Open Graph and SEO optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import type { Metadata } from "next";
 import { LandingPage } from "@/templates/landing-page";
 
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Venda seus produtos como afiliado em um único lugar",
+  robots: "index, follow",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-blog-phi-lyart.vercel.app/"
+  }
+};
+
 export default function HomePage() {
-  return (
-    <>
-      <LandingPage />
-    </>
-  );
+  return <LandingPage />;
 }
