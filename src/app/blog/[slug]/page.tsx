682d01b92cee9eb1f648f8403eadef493cf53f51
Matheus Veiga Lima <mveigadelima@gmail.com>
2026-09-25 09:36:43 -0300

    feat: implement metadata generation for blog posts and enhance layout Open Graph support

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from "next";
+
 import { PostPage } from "@/templates/blog/post-page";
 import { allPosts } from "contentlayer/generated";
 import { notFound } from "next/navigation";
@@ -7,6 +9,29 @@ type BlogPostPageProps = {
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) return {};
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [
+      {
+        name: post.author.name
+      }
+    ],
+    robots: "index, follow",
+    openGraph: {
+      images: [post.image]
+    }
+  };
+}
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,23 @@ import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Site.set",
-  description: "Venda seus produtos como afiliado em um único lugar"
+  description: "Venda seus produtos como afiliado em um único lugar",
+  openGraph: {
+    title: "Site.set",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://site-blog-phi-lyart.vercel.app/og-image.png",
+    siteName: "Site.set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-phi-lyart.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Site.set"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
