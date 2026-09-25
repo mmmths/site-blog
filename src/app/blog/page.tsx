ca5432194c036ec996fa27f8e7167cdcbf2f6ead
Matheus Veiga Lima <mveigadelima@gmail.com>
2026-09-25 09:22:17 -0300

    feat: add metadata for blog and layout pages to enhance SEO and Open Graph support

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
+import type { Metadata } from "next";
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://site-blog-phi-lyart.vercel.app/og-image.png",
+    siteName: "Blog",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://site-blog-phi-lyart.vercel.app/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Site.Set"
+      }
+    ]
+  }
+};
 
 export default function BlogLIstPage() {
   const sortedPosts = allPosts.sort(
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+import type { Metadata } from "next";
 import { Layout } from "@/components/layout";
 import "@/styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Site.set",
+  description: "Venda seus produtos como afiliado em um único lugar"
+};
 
 export default function RootLayout({
   children
